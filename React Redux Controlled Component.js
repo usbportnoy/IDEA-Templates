18bcca8b6aae12fd64889b3d83109b752a234b00
Jason <jason@lionleaf.com>
2017-10-17 17:19:07 -0400

    adding initial file

---
+++ React Redux Controlled Component.js
@@ -0,0 +1,28 @@
+import * as React from "react";
+import {connect} from "react-redux";
+
+class ${NAME} extends React.Component {
+	    constructor(props){
+		            super(props);
+		        }
+
+	    render(){
+		            return(
+				        
+				            )
+		        }
+}
+
+function mapStateToProps(state) {
+	    return {
+
+		        };
+}
+
+function mapDispatchToProps(dispatch) {
+	    return ({
+
+		        });
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(${NAME});
